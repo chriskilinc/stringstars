28650130da381d8deffb23d8f52d8aefe512a23b
Christopher Kilinc <chriskilinc@gmail.com>
2024-06-03 23:08:14 +0200

    Initial Commit
    A simple world, with a player, rain and some lights

---
+++ scripts/player.cs
@@ -0,0 +1,67 @@
+using Godot;
+using System;
+
+public partial class player : CharacterBody2D
+{
+	public AnimatedSprite2D animatedSprite;
+	public const float Speed = 150.0f;
+	public const float JumpVelocity = -350.0f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+	public override void _Ready()
+	{
+		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y += gravity * (float)delta;
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+			velocity.Y = JumpVelocity;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+
+		// Animations
+		if (IsOnFloor())
+		{
+			if (direction.X != 0)
+			{
+				animatedSprite.Play("run");
+			}
+			else
+			{
+				animatedSprite.Play("idle");
+			}
+		}
+		else
+		{
+			// animatedSprite.Play("Jump");
+		}
+
+		// Flip the sprite based on the direction.
+		if (direction.X != 0)
+			animatedSprite.FlipH = direction.X < 0;
+
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
